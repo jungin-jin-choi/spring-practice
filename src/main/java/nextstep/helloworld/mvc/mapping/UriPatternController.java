c1988990dc4ebcea152469578e8b4c4b35ef6e97
JungIn Choi <luvimperfection@gmail.com>
2021-10-13 07:49:27 +0900

    Add @RequestMapping and its variants

--- src/main/java/nextstep/helloworld/mvc/mapping/UriPatternController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/UriPatternController.java
@@ -2,6 +2,7 @@ package nextstep.helloworld.mvc.mapping;
 
 import nextstep.helloworld.mvc.domain.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/uri-pattern")
 public class UriPatternController {
 
+    @GetMapping("/users/{id}")
     public ResponseEntity<User> pathVariable(Long id) {
         User user = new User(id, "이름", "email");
         return ResponseEntity.ok().body(user);
     }
 
+    @GetMapping("/patterns/?")
     public ResponseEntity<String> pattern() {
         return ResponseEntity.ok().body("pattern");
     }
 
+    @GetMapping("/patterns/**")
     public ResponseEntity<String> patternStars() {
         return ResponseEntity.ok().body("pattern-multi");
     }
