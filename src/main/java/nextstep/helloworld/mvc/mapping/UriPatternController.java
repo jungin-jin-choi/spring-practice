cb5d0bf1309f00e7c8d997433209ca76a3ea4f57
JungIn Choi <luvimperfection@gmail.com>
2021-10-25 06:07:42 +0900

    spring mvc rough cloning

--- src/main/java/nextstep/helloworld/mvc/mapping/UriPatternController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/UriPatternController.java
@@ -3,6 +3,7 @@ package nextstep.helloworld.mvc.mapping;
 import nextstep.helloworld.mvc.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UriPatternController {
 
     @GetMapping("/users/{id}")
-    public ResponseEntity<User> pathVariable(Long id) {
+    public ResponseEntity<User> pathVariable(@PathVariable Long id) {
         User user = new User(id, "이름", "email");
         return ResponseEntity.ok().body(user);
     }
